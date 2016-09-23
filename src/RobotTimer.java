9fc0f6dab639052bf6a6989d715a2f948bc8b96c
Thom Renström[filter lfs] <thomrenstrom@hotmail.se>
2016-09-23 16:36:49 +0200

    jebane

---
+++ src/RobotTimer.java
@@ -0,0 +1,22 @@
+/**
+ * Created by sejiaw on 2016-09-23.
+ */
+import java.util.Timer;
+public class RobotTimer {
+
+    RobotTimer(){
+
+    }
+    public void clockStart(){
+
+    }
+    public void clockStop(){
+
+    }
+    public void endTime(boolean isEnd){
+        //stoppa klockan
+        //clockStop();
+        //skriv ut tid
+
+    }
+}
