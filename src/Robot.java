e2d46b149aa2cb85db6dba848d43ead9cbbb0221
Thom Renström[filter lfs] <thomrenstrom@hotmail.se>
2016-09-26 15:03:58 +0200

    jebane

--- src/Robot.java
+++ src/Robot.java
@@ -2,4 +2,31 @@
  * Created by sejiaw on 2016-09-23.
  */
 public class Robot {
+
+
+    //attribute
+    Position myPos;
+    Position nextPos;
+    boolean isDone;
+
+
+    public Robot(){
+
+    }
+
+    private void createRobot(){
+        //get startpos
+        //set startpos
+        //install robot?
+
+    }
+
+    private void runRot(){
+
+        while(!isDone){
+            System.out.println("JEBANE");
+        }
+
+    }
+
 }
