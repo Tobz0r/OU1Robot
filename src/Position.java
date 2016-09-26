300ad3d9f45f0621207368b988cc514dfc967ac3
Tobias Estefors <dv13tes@cs.umu.se>
2016-09-26 14:35:35 +0200

    jebane

--- src/Position.java
+++ src/Position.java
@@ -2,4 +2,30 @@
  * Created by sejiaw on 2016-09-23.
  */
 public class Position {
+
+
+    double x,y;
+
+    public Position(double x, double y){
+        this.x=x;
+        this.y=y;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+
 }
--- src/Reader.java
+++ src/Reader.java
@@ -1,5 +1,37 @@
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.*;
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * Created by sejiaw on 2016-09-23.
  */
 public class Reader {
+
+    public Reader(){
+
+    }
+
+    public void readPath() throws FileNotFoundException, JSONException {
+        int nPoints;
+        JSONObject obj = new JSONObject("Path-around-table.json");
+
+        Position[] path;
+// read the path from the file
+        nPoints = data.size();
+        path = new Position[nPoints];
+        int index = 0;
+        for (Map<String, Object> point : data)
+        {
+            Map<String, Object> pose = (Map<String, Object>)point.get("Pose");
+            Map<String, Object> aPosition = (Map<String, Object>)pose.get("Position");
+            double x = (Double)aPosition.get("X");
+            double y = (Double)aPosition.get("Y");
+            path[index] = new Position(x, y);
+            index++;
+        }
+
+    }
 }
