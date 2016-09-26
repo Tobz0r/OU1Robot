731f9dd918931f30283dd1c1246fa4cf48093ac2
Thom Renström[filter lfs] <thomrenstrom@hotmail.se>
2016-09-26 16:57:07 +0200

    jebane

--- src/Controls.java
+++ src/Controls.java
@@ -1,7 +1,56 @@
+
+
 /**
  * Created by sejiaw on 2016-09-23.
  */
+import java.lang.Math;
+
 public class Controls {
 
+
+    private double angSpeed,lineSpeed,xTurnRate, yTurnRate,distance;
+    RobotCommunication robotCom;
+
+
+    public Controls(RobotCommunication robotC){
+        this.robotCom = robotC;
+    }
+
+    /**
+     * Calculate angle for robot to turn
+     */
+    public void calculateAngle(){
+
+    }
+
+    /**
+     * Set turnrate for controls
+     * @param x value to turn
+     * @param y value to turn
+     */
+    public void setTurnRate(double x, double y){
+        this.xTurnRate = x;
+        this.yTurnRate = y;
+
+    }
+
+    /**
+     * Set speed for controls
+     * @param angSpeed angular speed
+     * @param lineSpeed lineer speed.
+     */
+    public void setSpeed(double angSpeed, double lineSpeed){
+        this.angSpeed=angSpeed;
+        this.lineSpeed=lineSpeed;
+
+    }
+
+    /**
+     * Set distance for controls
+     * @param distance distance to travel
+     */
+    public void setDistance(double distance){
+        this.distance = distance;
+    }
 
 }
