bc930a56e10c2ac23343bab3c4621abbc4c2fbab
Tobias Estefors <dv13tes@cs.umu.se>
2016-09-26 16:59:37 +0200

    eliashej

--- src/HttpRequester.java
+++ src/HttpRequester.java
@@ -1,69 +1,59 @@
 
-import com.fasterxml.jackson.databind.util.JSONPObject;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
-import java.io.InputStreamReader;
+import org.json.JSONObject;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-import javax.net.ssl.HttpsURLConnection;
 
 public class HttpRequester {
 
-    private final String robot_url="localhost:50000";
+    private final String robot_url="http://localhost:50000";
+
 
     // HTTP GET request
-    private void sendGet() throws Exception {
+    private void requestGET() throws Exception {
 
         URL obj = new URL(robot_url+"/lokarria/localization");
         HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         // optional default is GET
         con.setRequestMethod("GET");
-        int responseCode = con.getResponseCode();
-        System.out.println("Response Code : " + responseCode);
+        if(con.getResponseCode()!=200){
+            System.out.println("GET ERROR");
+        }
         BufferedReader in = new BufferedReader(
                 new InputStreamReader(con.getInputStream()));
         String inputLine;
         StringBuffer response = new StringBuffer();
         while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        //    response.append(inputLine);
+            System.out.println(inputLine);
+
         }
         in.close();
-        System.out.println(response.toString());
+       // System.out.println(response);
     }
 
     // HTTP POST request
-    private void sendPost(int TargetAngularSpeed, int TargetLinearSpeed) throws Exception {
+    private void requestPOST(double TargetAngularSpeed, double TargetLinearSpeed) throws Exception {
         URL obj = new URL(robot_url+"/lokarria/differentialdrive");
-        HttpsURLConnection con = (HttpsURLConnection) obj.openConnection();
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
         //add reuqest header
         con.setRequestMethod("POST");
         con.setRequestProperty("Content-type","application/json");
         con.setRequestProperty("Accept", "text/json");
-
-        String urlParameters = "TargetAngularSpeed="+TargetAngularSpeed+"&TargetLinearSpeed="+TargetLinearSpeed;
-
         // Send post request
+        JSONObject param = new JSONObject();
+        param.put("TargetAngularSpeed",TargetAngularSpeed);
+        param.put("TargetLinearSpeed", TargetLinearSpeed);
         con.setDoOutput(true);
         DataOutputStream wr = new DataOutputStream(con.getOutputStream());
-        wr.writeBytes(urlParameters);
+        wr.writeBytes(param.toString());
         wr.flush();
         wr.close();
-
-        int responseCode = con.getResponseCode();
-        System.out.println("Post parameters : " + urlParameters);
-        System.out.println("Response Code : " + responseCode);
-        BufferedReader in = new BufferedReader(
-                new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
+        if (con.getResponseCode()!=204){
+            System.out.println("POST ERROR!!");
         }
-        in.close();
-        System.out.println(response.toString());
     }
 
 }
